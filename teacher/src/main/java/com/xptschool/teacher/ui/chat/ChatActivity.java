5047950b4eaaefa49f82967199d81c180eb89ebc
kongdexing <il>
2017-05-11 18:08:09 +0800

    teacher:impl receiver chat message in ChatActivity

--- teacher/src/main/java/com/xptschool/teacher/ui/chat/ChatActivity.java
+++ teacher/src/main/java/com/xptschool/teacher/ui/chat/ChatActivity.java
@@ -179,35 +179,45 @@ public class ChatActivity extends BaseActivity {
         @Override
         public void onReceive(Context context, Intent intent) {
             Bundle bundle = intent.getExtras();
-            BaseMessage sendMsg = null;
-            if (bundle != null) {
-                sendMsg = (BaseMessage) bundle.get("message");
+            if (bundle == null) {
+                return;
             }
-
-            BeanChat chat = new BeanChat();
-            chat.parseMessageToChat(sendMsg);
-            chat.setHasRead(true);
-            chat.setTime(CommonUtil.getCurrentDateHms());
-
             String action = intent.getAction();
             Log.i(TAG, "onReceive: " + action);
-            if (action.equals(BroadcastAction.MESSAGE_SEND_START)) {
-                chat.setSendStatus(ChatUtil.STATUS_SENDING);
-                adapter.addData(chat);
-                recycleView.smoothScrollToPosition(adapter.getItemCount());
-                GreenDaoHelper.getInstance().insertChat(chat);
-            } else if (action.equals(BroadcastAction.MESSAGE_SEND_SUCCESS)) {
-                chat.setSendStatus(ChatUtil.STATUS_SUCCESS);
-                adapter.updateData(chat);
-                GreenDaoHelper.getInstance().updateChat(chat);
 
-            } else if (action.equals(BroadcastAction.MESSAGE_SEND_FAILED)) {
-                chat.setSendStatus(ChatUtil.STATUS_FAILED);
-                adapter.updateData(chat);
-                GreenDaoHelper.getInstance().updateChat(chat);
+            if (action.equals(BroadcastAction.MESSAGE_RECEIVED)) {
+                BeanChat chat = (BeanChat) bundle.getSerializable("chat");
+                Log.i(TAG, "onReceive: " + chat.getParentId() + " " + parent.getUser_id() + "  " + chat.getContent());
+                //判断是否为当前正在聊天家长发来的信息
+                if (chat.getParentId().equals(parent.getUser_id())) {
+                    adapter.addData(chat);
+                    recycleView.smoothScrollToPosition(adapter.getItemCount());
+                }
+            } else {
+                BaseMessage sendMsg = (BaseMessage) bundle.get("message");
 
+                BeanChat chat = new BeanChat();
+                chat.parseMessageToChat(sendMsg);
+                chat.setHasRead(true);
+                chat.setTime(CommonUtil.getCurrentDateHms());
+
+                if (action.equals(BroadcastAction.MESSAGE_SEND_START)) {
+                    chat.setSendStatus(ChatUtil.STATUS_SENDING);
+                    adapter.addData(chat);
+                    recycleView.smoothScrollToPosition(adapter.getItemCount());
+                    GreenDaoHelper.getInstance().insertChat(chat);
+                } else if (action.equals(BroadcastAction.MESSAGE_SEND_SUCCESS)) {
+                    chat.setSendStatus(ChatUtil.STATUS_SUCCESS);
+                    adapter.updateData(chat);
+                    GreenDaoHelper.getInstance().updateChat(chat);
+
+                } else if (action.equals(BroadcastAction.MESSAGE_SEND_FAILED)) {
+                    chat.setSendStatus(ChatUtil.STATUS_FAILED);
+                    adapter.updateData(chat);
+                    GreenDaoHelper.getInstance().updateChat(chat);
+
+                }
             }
-
         }
     };
 
