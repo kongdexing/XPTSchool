d276d8fca4042f26231000d07385efdf64174a08
kongdexing <il>
2017-07-31 16:06:08 +0800

    家长端-修改token上传参数

--- parent/src/main/java/com/xptschool/parent/push/UpushTokenHelper.java
+++ parent/src/main/java/com/xptschool/parent/push/UpushTokenHelper.java
@@ -49,12 +49,13 @@ public class UpushTokenHelper {
         VolleyHttpService.getInstance().sendPostRequest(HttpAction.HOOK_PUSH_TOKEN,
                 new VolleyHttpParamsEntity()
                         .addParam("status", "1")    //1登录 2其他(切换\退出)
-                        .addParam("mobilenum", parent.getLoginName())
-                        .addParam("mobiletype", "1") //1Android 0ios
-                        .addParam("parentid", parent.getU_id())
-                        .addParam("devicetoken", device_token)
-                        .addParam("mobilemodel", android.os.Build.MODEL)
-                        .addParam("pushtype", push)
+                        .addParam("user_name", parent.getLoginName())
+                        .addParam("system_model", "1") //1Android 0ios
+                        .addParam("user_id", parent.getU_id())
+                        .addParam("device_token", device_token)
+                        .addParam("mobile_model", android.os.Build.MODEL)
+                        .addParam("push_name", push)
+                        .addParam("user_type", "4")
                 , null);
     }
 
@@ -71,9 +72,10 @@ public class UpushTokenHelper {
         VolleyHttpService.getInstance().sendPostRequest(HttpAction.HOOK_PUSH_TOKEN,
                 new VolleyHttpParamsEntity()
                         .addParam("status", "2")    //1登录 2其他(切换\退出)
-                        .addParam("mobilenum", parent.getLoginName())
-                        .addParam("mobiletype", "1") //1Android 0ios
-                        .addParam("parentid", parent.getU_id())
+                        .addParam("user_name", parent.getLoginName())
+                        .addParam("system_model", "1") //1Android 0ios
+                        .addParam("user_id", parent.getU_id())
+                        .addParam("user_type", "4")
                 , null);
 
     }
