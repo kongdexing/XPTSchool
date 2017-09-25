e461124a80f69c0feb75175ec822f4fa0275f3d0
xuyancheng <il>
2017-09-25 15:28:03 +0800

    初步实现第一画-左饼图

--- anfang-report/src/main/java/com/shuhai/anfang/report/ReportApplication.java
+++ anfang-report/src/main/java/com/shuhai/anfang/report/ReportApplication.java
@@ -3,6 +3,8 @@ package com.shuhai.anfang.report;
 import android.app.Application;
 import android.support.v7.app.AppCompatActivity;
 
+import com.android.volley.common.VolleyHttpService;
+
 /**
  * Created by dexing on 2017/9/25 0025.
  * No1
@@ -11,4 +13,10 @@ import android.support.v7.app.AppCompatActivity;
 public class ReportApplication extends Application {
 
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        VolleyHttpService.init(this);
+    }
 }
--- anfang-report/src/main/java/com/shuhai/anfang/report/ui/MainActivity.java
+++ anfang-report/src/main/java/com/shuhai/anfang/report/ui/MainActivity.java
@@ -1,12 +1,25 @@
 package com.shuhai.anfang.report.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.view.WindowManager;
 
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.shuhai.anfang.report.R;
 
+import java.util.ArrayList;
+
 public class MainActivity extends BaseActivity {
+
+    private PieChart mChart;
+    private Typeface tf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +27,101 @@ public class MainActivity extends BaseActivity {
 
         setContentView(R.layout.activity_main);
 
+        mChart = (PieChart) findViewById(R.id.chart1);
+        mChart.setUsePercentValues(true);
+        mChart.getDescription().setEnabled(false);
+
+//        mChart.setDragDecelerationFrictionCoef(0.95f);
+
+        tf = Typeface.createFromAsset(getAssets(), "OpenSans-Regular.ttf");
+
+        mChart.setCenterTextTypeface(Typeface.createFromAsset(getAssets(), "OpenSans-Light.ttf"));
+//        mChart.setCenterText(generateCenterSpannableText());
+
+        //外部间距
+        mChart.setExtraOffsets(20.f, 0.f, 20.f, 0.f);
+
+        //绘制中心圆
+        mChart.setDrawHoleEnabled(true);
+        mChart.setHoleColor(getResources().getColor(R.color.color_hole));
+
+//        mChart.setTransparentCircleColor(Color.WHITE);
+//        mChart.setTransparentCircleAlpha(110);
+
+        //中心园半径
+        mChart.setHoleRadius(58f);
+        mChart.setTransparentCircleRadius(61f);
+
+        mChart.setDrawCenterText(true);
+
+        mChart.setRotationAngle(0);
+        // enable rotation of the chart by touch
+        mChart.setRotationEnabled(true);
+        mChart.setHighlightPerTapEnabled(true);
+
+        // mChart.setUnit(" €");
+        // mChart.setDrawUnitsInChart(true);
+
+        // add a selection listener
+//        mChart.setOnChartValueSelectedListener(this);
+
+        setData(2, 100);
+
+//        mChart.animateY(1400, Easing.EasingOption.EaseInOutQuad);
+        // mChart.spin(2000, 0, 360);
+
+        Legend l = mChart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        l.setOrientation(Legend.LegendOrientation.VERTICAL);
+        l.setDrawInside(false);
+        l.setEnabled(false);
 
     }
+
+    private void setData(int count, float range) {
+
+        float mult = range;
+
+        ArrayList<PieEntry> entries = new ArrayList<PieEntry>();
+
+        // NOTE: The order of the entries when being added to the entries array determines their position around the center of
+        // the chart.
+        entries.add(new PieEntry((float) (Math.random() * mult) + mult / 5, ""));
+        entries.add(new PieEntry((float) (Math.random() * mult) + mult / 5, ""));
+
+        PieDataSet dataSet = new PieDataSet(entries, "学生卡总量统计");
+//        dataSet.setSliceSpace(0f);
+//        dataSet.setSelectionShift(0f);
+
+        // add a lot of colors
+
+        ArrayList<Integer> colors = new ArrayList<Integer>();
+
+        colors.add(getResources().getColor(R.color.color_used));
+        colors.add(getResources().getColor(R.color.color_unused));
+
+        dataSet.setColors(colors);
+        //dataSet.setSelectionShift(0f);
+
+        dataSet.setValueLinePart1OffsetPercentage(70.f);
+        dataSet.setValueLinePart1Length(0.3f);
+        dataSet.setValueLinePart2Length(0.5f);
+        dataSet.setValueLineColor(Color.WHITE);
+        //dataSet.setXValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);
+        dataSet.setYValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);
+
+        PieData data = new PieData(dataSet);
+        data.setValueFormatter(new PercentFormatter());
+        data.setValueTextSize(12f);
+        data.setValueTextColor(Color.BLACK);
+        data.setValueTypeface(tf);
+        mChart.setData(data);
+
+        // undo all highlights
+        mChart.highlightValues(null);
+        mChart.invalidate();
+    }
+
+
 }
